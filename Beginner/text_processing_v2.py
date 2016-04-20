cc6fe4f1cf36cd63579fd853040f2bd45243982e
zhengxia-qin <zhengxia.qin@gmail.com>
2016-04-20 13:51:11 +0200

    Update text_processing_v2.py

--- Beginner/text_processing_v2.py
+++ Beginner/text_processing_v2.py
@@ -1,7 +1,7 @@
 listC = []
 with open('FileA.txt', 'r') as read_file_A:
 	with open('FileB.txt', 'r') as read_file_B:
-		listA = read_file_A.read().splitlines() 
+		listA = read_file_A.read().splitlines()  #method splitlines() returns a list with all the lines in string
 		listB = read_file_B.read().splitlines()
 		for i in range(len(listA)):
 			listC.append(listA[i].lower() + " eat " + listB[i]) 
